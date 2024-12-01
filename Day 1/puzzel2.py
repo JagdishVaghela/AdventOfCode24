07d281ca73c6043733c83f31c1a7d2b0a6ac305d
JagdishVaghela <vjagdishp4@gmail.com>
2024-12-01 12:32:38 +0000

    puzzel 2 code

---
+++ Day 1/puzzel2.py
@@ -0,0 +1,18 @@
+f = open("p1input.txt", "r")
+list1 = []
+list2 = []
+total = 0
+for row in f:
+    int1 = int(row[0:5])
+    int2 = int(row[8:13])
+    list1.append(int1)
+    list2.append(int2)
+
+for i in range(0,1000):
+    count = 0
+    for j in range(0,1000):
+        if list1[i] == list2[j]:
+            count += 1
+    total += (count * list1[i])
+
+print(total)
