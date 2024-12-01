46098bab490931a375d60322713320ceff3525b4
JagdishVaghela <vjagdishp4@gmail.com>
2024-12-01 12:32:25 +0000

    puzzel 1 code

---
+++ Day 1/puzzel1.py
@@ -0,0 +1,18 @@
+f = open("p1input.txt", "r")
+list1 = []
+list2 = []
+total = 0
+for row in f:
+    int1 = int(row[0:5])
+    int2 = int(row[8:13])
+    list1.append(int1)
+    list2.append(int2)
+
+list1.sort()
+list2.sort()
+
+for i in range (0,1000):
+      dist = abs(list1[i]-list2[i])
+      total = total + dist
+
+print(total)
